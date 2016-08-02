2bf70b515b78521f9f12ada0447d6442bcbc1e4d
Séra Bálint <sera.balint@e-vista.hu>
2016-08-02 17:39:57 +0300

    update user endpoint finished

--- logger.go
+++ logger.go
@@ -1,2 +1,17 @@
 package main
 
+import (
+    "log"
+    "os"
+)
+
+var (
+	Log      *log.Logger
+)
+
+func init() {
+    file, _ := os.OpenFile("go-log.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+    Log = log.New(file,
+    "TWITTER-EPUB: ",
+    log.Ldate|log.Ltime|log.Lshortfile)
+}
